8c3425f3a8759243f20a9bd400828fcb0dcda22a
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2023-10-15 21:48:21 +0800

    Updated

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\{HomeController, AboutController};
+
+
 
 $app = new App();
-
-$app->get('/');
-
-dd($app);
+ 
+$app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
